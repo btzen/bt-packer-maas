97a386818e44e52c7e9acc9662d6c3674fc33fe1
imsfc <imsfc9@gmail.com>
2025-07-17 16:59:50 +0800

    修复安装packer的gpg公钥问题

--- install_deps.sh
+++ install_deps.sh
@@ -2,7 +2,7 @@
 
 # 安装 Packer
 # https://developer.hashicorp.com/packer/tutorials/docker-get-started/get-started-install-cli
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install packer
 
